20598b31799a6d99590e7aa21290474f537a4e77
Jeff Ancel <jancel@gmail.com>
2017-12-10 09:25:09 -0500

    Integrated more contentful, fix issue with id's coming through on env OBJ

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <site-header :socialIcons="socialIcons" :siteLogo="logo" />
+    <site-header :siteLogo="logo" :socialIcons="socialIcons" :siteInfo="siteInfo" :contactInfos="contactInfo" />
     <!-- <home-page /> -->
   </div>
 </template>
@@ -30,23 +30,35 @@ export default {
     HomePage
   },
   // `env` is available in the context object
-  asyncData (context) {
-    const env = context.env
+  asyncData () {
+    let siteLogo = client.getAssets().then((assets) => { return assets.items.filter(logo => logo.fields.title === 'Logo').slice(-1)[0].fields })
+    // let siteInfo = client.getEntry(env.SITE_ENTRY_ID).then((siteInfo) => { return siteInfo })
     return Promise.all([
       // fetch the owner
-      client.getAssets(),
+      siteLogo,
       // fetch all social icons sorted by creation date
       client.getEntries({
-        'content_type': env.CTF_SOCIAL_ICON_TYPE_ID,
+        'content_type': 'socialIcons',
         order: 'sys.createdAt'
+      }),
+      // siteInfo
+      // fetch siteInfo
+      client.getEntry('u4rwFdNsI0Iw8ayyw4Yi6'),
+      client.getEntries({
+        'content_type': 'contactInfo'
       })
-    ]).then(([assets, socialIcons]) => {
+    ]).then(([logo, socialIcons, siteInfo, contactInfo]) => {
       // return site logo (last contentful Logo upload)
-      let siteLogo = assets.items.filter(logo => logo.fields.title === 'Logo').slice(-1)[0].fields
-
+      console.log('logo', logo)
+      console.log('socialIcons', socialIcons)
+      console.log('contactInfo', contactInfo.items)
+      console.log('siteInfo', siteInfo.fields)
+      // let siteLogo = assets.items.filter(logo => logo.fields.title === 'Logo').slice(-1)[0].fields
       return {
-        logo: siteLogo,
-        socialIcons: socialIcons.items
+        logo: logo,
+        socialIcons: socialIcons.items,
+        siteInfo: siteInfo.fields,
+        contactInfo: contactInfo.items
       }
     }).catch(console.error)
   }
